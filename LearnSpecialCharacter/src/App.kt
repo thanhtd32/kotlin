24aca2dc9b95aa2ac48c585ee2cfa49b52f37966
thanhtd32 <53929436+thanhtd32@users.noreply.github.com>
2021-07-17 15:42:12 +0900

    Add files via upload

---
+++ LearnSpecialCharacter/src/App.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+   println("It sits, poisonous")
+   println("Dripping sorrow over the windowsill")
+   println("\tI drove to the \"Skyway\"")
+   println("\tDropped a heart over the edge.")
+   print("Watched it splash under")
+   print("\nIt took a couple seconds to hit.")
+}
