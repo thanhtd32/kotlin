ff2d88eab539f524d78f6039a9b83ef94c5977e9
thanhtd32 <53929436+thanhtd32@users.noreply.github.com>
2021-08-27 14:43:34 +0900

    Add files via upload

---
+++ LearnPrimaryConstructor/src/Student.kt
@@ -0,0 +1,6 @@
+class Student (code:Int,name:String){
+    init {
+        println("This is primary constructor")
+        println("Code=$code ; Name =$name")
+    }
+}
---
+++ LearnPrimaryConstructor/src/TestStudent.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    //Create an object with name john
+    var john=Student(100,"John Barrot")
+    //Create an object with name peter
+    //Specify the data type
+    var peter:Student=Student(200,"Peter Christal")
+}
